b6fe769f48acd7167bd684fde5116de17025f2bf
Bg735 <f.lanzara04@gmail.com>
2024-12-10 13:51:49 +0000

    Corrected naming error in SimpleEmailChecker.java

--- addressbook/src/main/java/it/unisa/diem/Model/Interfaces/Checker/SimpleEmailChecker.java
+++ addressbook/src/main/java/it/unisa/diem/Model/Interfaces/Checker/SimpleEmailChecker.java
@@ -3,7 +3,7 @@ package it.unisa.diem.Model.Interfaces.Checker;
 /**
  * EmailChecker base implementation, used to check if a certain string is a valid email.
  */
-public class SimpleEmailchecker implements EmailChecker {
+public class SimpleEmailChecker implements EmailChecker {
     
     /**
      * @copydoc EmailChecker::check()
