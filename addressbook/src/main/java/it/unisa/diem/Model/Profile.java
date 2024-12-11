4eaef82811ecc12a6f24d52f70574749ce24d1d9
39342 <39342@pcelettra.lan>
2024-12-11 10:52:53 +0100

    class Profile

--- addressbook/src/main/java/it/unisa/diem/Model/Profile.java
+++ addressbook/src/main/java/it/unisa/diem/Model/Profile.java
@@ -5,6 +5,12 @@ import java.io.Serializable;
 
 import it.unisa.diem.Model.Interfaces.Checker.CharacterLimitStringChecker;
 import it.unisa.diem.Model.Interfaces.Checker.ItalianPhoneChecker;
+import it.unisa.diem.Utility.FileManager;
+import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
@@ -12,7 +18,7 @@ import javafx.beans.property.StringProperty;
  * Represents a profile in the application.
  * 
  * A profile is characterized by a name, a phone number, a profile picture and an address book. Only the name field is mandatory.
- * The name is subject to a maximum length of {@link #MAX_NAMELEN} characters.
+ * The name is subject to a maximum length of {@value #MAX_NAMELEN} characters.
  * The phone number is subject to the constraints of {@link ItalianPhoneChecker}.
  * It is created to grant full compatibility with a JavaFX UI
  * 
@@ -37,14 +43,18 @@ public class Profile implements Serializable{
      * @param profilePicture
      * @param addressBookPath
      */
-    public Profile(StringProperty name, StringProperty phone, StringProperty profilePicture, StringProperty addressBookPath) {
+    public Profile(StringProperty name, StringProperty phone, StringProperty profilePicture, StringProperty addressBookPath) throws IOException {
         // Constructor implementation
+        this.name = name; 
+        this.phone = phone; 
+        setProfilePicture(profilePicture.get()); 
+        setAddressBookPath(addressBookPath.get()); 
+        
     }
 
     /**
      * Returns the name of the profile.
      * 
-     * @pre name!=null
      * @pre !name.isEmpty()
      * @post getName().equals(name)
      * @param[in] name the new name of the contact
@@ -55,27 +65,31 @@ public class Profile implements Serializable{
     }
 
     /**
-     * Sets the name of the profile, given that it satisfies the satisfies the condition of {@link CharacterLimitStringChecker} (character limit is set to {@link #MAX_NAMELEN}).
+     * Sets the name of the profile, given that it satisfies the satisfies the condition of {@link CharacterLimitStringChecker} (character limit is set to {@value #MAX_NAMELEN}).
      * 
      * @param name the new name of the profile
      * @return true if the name is valid
      *         false otherwise
      */
     public boolean setName(String name) {
-        // TODO: Implement this method
+        CharacterLimitStringChecker checker = new CharacterLimitStringChecker(MAX_NAMELEN); 
+        if(checker.check(name)) {
+            this.name = new SimpleStringProperty(name); 
+            return true; 
+        }
+        return false; 
     }
 
     /**
      * Returns the phone number of the profile.
      * 
-     * @pre phone!=null
      * @pre !phone.isEmpty()
      * @post getPhone().equals(phone)
      * @param[in] phone the new phone number of the profile
      * @return the phone number of the profile
      */
     public StringProperty getPhone() {
-        // TODO: Implement this method
+        return phone; 
     }
 
     /**
@@ -85,18 +99,22 @@ public class Profile implements Serializable{
      * @return true if the phone number is valid
      *         false otherwise
      */
-    public boolean setPhone(int phone) {
-        // TODO: Implement this method
+    public boolean setPhone(String phone) {
+        ItalianPhoneChecker checker = new ItalianPhoneChecker(); 
+        if(checker.check(phone)) {
+            this.phone = new SimpleStringProperty(phone); 
+            return true; 
+        }
+        return false; 
     }
 
     /**
      * Returns the internal path of the picture of the Contact.
      * 
-     * @invariant picture!=null
      * @return internal relative path to the picture of the Contact
      */
     public String getProfilePicture() {
-        // TODO: Implement this method
+        return profilePicture;
     }
 
     /**
@@ -115,17 +133,29 @@ public class Profile implements Serializable{
      * @throws IOException if the specified path is not valid
      */
     public void setAddressBookPath(String addressBookPath) throws IOException {
+        Path path = Paths.get(addressBookPath);
+        if(!Files.exists(path)) {
+            throw new IOException("File: " + addressBookPath + " does not exist.");
+        }
         this.addressBookPath = addressBookPath;
     }
 
     /**
      * Takes the picture at the specified path and copies it in the application's assets folder, then assigns it as the profile picture.
      * 
-     * @param[in] picture the path of the picture to assign to the contact
-     * @throws IOException if the picture cannot be copied in the assets folder, or the specified path is not valid
+     * @param[in] profilePicture the path of the picture to assign to the contact
+     * @throws FileNotFoundException if the picture cannot be copied in the assets folder, or the specified path is not valid
      */
-    public void setProfilePicture(String profilePicture) throws IOException {
-        // TODO: Implement this method
+    public void setProfilePicture(String profilePicture) throws FileNotFoundException {
+        Path profilePicturePath = Paths.get(profilePicture);
+        if(!Files.exists(profilePicturePath)) {
+            throw new FileNotFoundException("File: " + profilePicture + " does not exist.");
+        }
+        
+        String fileName = profilePicturePath.getFileName().toString();
+        String extension = fileName.substring(fileName.lastIndexOf('.')+1); 
+        Path destinationPath = Paths.get(FileManager.generateProfilePicturePath(extension)); 
+        this.profilePicture = Files.copy(profilePicturePath, destinationPath, StandardCopyOption.REPLACE_EXISTING).toString(); 
     }
 
     private void writeObject(java.io.ObjectOutputStream out) throws IOException {
