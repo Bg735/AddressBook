eab88b0d3705385cd3a83a134de3512f8221c04f
Bg735 <f.lanzara04@gmail.com>
2024-12-13 16:04:42 +0000

    Implement classes to start application
    Co-authored-by: Mauro03av <Mauro03av@users.noreply.github.com>

--- addressbook/src/main/java/it/unisa/diem/Utility/SceneManager.java
+++ addressbook/src/main/java/it/unisa/diem/Utility/SceneManager.java
@@ -1,7 +1,14 @@
 package it.unisa.diem.Utility;
 
+import java.io.IOError;
+import java.io.IOException;
+
+import it.unisa.diem.AddressBookApplication;
 import it.unisa.diem.Controller.AddressBookController;
 import it.unisa.diem.Controller.ProfileSelectionController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * Utility class to manage the scene of the application.
@@ -9,30 +16,42 @@ import it.unisa.diem.Controller.ProfileSelectionController;
  */
 public class SceneManager {
 
-    ProfileSelectionController profileSelectionController=null; /**< The controller of the ProfileSelection view */
-    AddressBookController addressBookController=null; /**< The controller of the AddressBook view */
+    private static ProfileSelectionController profileSelectionController=null; /**< The controller of the ProfileSelection view */
+    private static AddressBookController addressBookController=null; /**< The controller of the AddressBook view */
+    private static Stage stage;
+    private static Scene scene;
 
     /**
      * Loads the view specified by the given FXML file.
      * @param fxml
      */
-    private static void loadScene(String fxml) {
-        // TODO: Implement this method
+    private static void loadScene(String fxml, Stage stage) throws IOException {
+        SceneManager.stage = stage;
+        FXMLLoader fxmlLoader = new FXMLLoader(AddressBookApplication.class.getResource(fxml + "View.fxml"));
+        try { 
+            fxmlLoader.setController(Class.forName("it.unisa.diem.Controller."+fxml.substring(0, 1).toUpperCase() + fxml.substring(1)+"Controller").getConstructor().newInstance());
+        } catch (Exception e) {
+            System.exit(1);
+        }
+        
+        SceneManager.scene = new Scene(fxmlLoader.load());
+        SceneManager.stage.setScene(scene);
+        stage.show();
     }
 
     /**
      * Loads the ProfileSelection view.
      * If the related controller has already been instantiated for the current session, it is reused.
      */
-    public static void loadProfileSelection() {
-        // TODO: Implement this method
+    public static void loadProfileSelection(Stage stage) throws IOException {
+        loadScene("ProfileSelection", stage);   
     }
 
     /**
      * Loads the AddressBook view.
      * If the related controller has already been instantiated for the current session, it is reused.
      */
-    public static void loadAddressBook() {
-        // TODO: Implement this method
+    public static void loadAddressBook(Stage stage) throws IOException {
+        loadScene("AddressBook", stage);
     }
 }
