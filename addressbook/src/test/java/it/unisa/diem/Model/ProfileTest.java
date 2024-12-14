482d1aedb3b87582f49840c3313ca44335a98228
landigf <g.landi83@studenti.unisa.it>
2024-12-14 12:46:21 +0100

    Ended Profile - mancano i test su addressbookPath

--- addressbook/src/test/java/it/unisa/diem/Model/ProfileTest.java
+++ addressbook/src/test/java/it/unisa/diem/Model/ProfileTest.java
@@ -15,44 +15,46 @@ public class ProfileTest {
     
     @BeforeEach
     public void setUp() throws IOException {
-        profile = new Profile(new SimpleStringProperty("profileName"), new SimpleStringProperty("1234567890"), new SimpleStringProperty("pic/path"), new SimpleStringProperty("address/book/path"));
+        profile = new Profile(new SimpleStringProperty("profileName"), new SimpleStringProperty("1234567890"), new SimpleStringProperty(""), new SimpleStringProperty(""));
     }
     
     @Test
-    public void setNameValidTest() {
+    public void setNameValidTest()  {
         assertTrue(profile.setName("ValidName"), "Name is valid."); 
     }
     
     @Test
     public void setNameInvalidTest() {
-        assertTrue(!profile.setName("abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"), "Name is invalid."); 
+        assertFalse(profile.setName("abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"), "Name is invalid."); 
     }
     
     
     @Test
     public void setPhoneValid1Test() {
-        assertTrue(profile.setName("1234567890"), "Phone is valid."); 
+        assertTrue(profile.setPhone("1234567890"), "Phone is valid."); 
     }
     
     @Test
     public void setPhoneValid2Test() {
-        assertTrue(profile.setName("123456789"), "Phone is valid."); 
+        assertTrue(profile.setPhone("123456789"), "Phone is valid."); 
     }
     
     @Test
     public void setPhoneInvalid1Test() {
-        assertTrue(profile.setName("12345"), "Phone is invalid."); 
+        assertFalse(profile.setPhone("12345"), "Phone is invalid."); 
     }
     
     @Test
     public void setPhoneInvalid2Test() {
-        assertTrue(profile.setName("1234567891011"), "Phone is invalid."); 
+        assertFalse(profile.setPhone("1234567891011"), "Phone is invalid."); 
     }
     
-    
+    /*
     @Test
     public void setAddressBookPathValidTest() {
-        assertThrows(IOException.class, () -> profile.setAddressBookPath("src/test/resources/address_books/address_book_1.obj"),"AddressBook path is valid. "); 
+        assertDoesNotThrow(() -> {
+            profile.setAddressBookPath("src/test/resources/address_books/address_book_1.obj");
+        }); 
     }
     
     @Test
@@ -70,5 +72,6 @@ public class ProfileTest {
     public void setProfilePictureInvalidTest() {
         assertThrows(IOException.class, () -> profile.setProfilePicture("src/test/resources/profile_pictures/doesNotExist.JPEG"), "Profile picture is invalid. ");
     }
+    */
     
 }
