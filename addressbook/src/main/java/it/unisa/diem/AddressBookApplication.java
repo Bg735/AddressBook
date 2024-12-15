27c73fe59d2ba9cff405041f77e705a9416d0ca5
Mauro03av <m.melillo34@studenti.unisa.it>
2024-12-15 20:39:47 +0100

    Uptaded AddressBookApplication.java

--- addressbook/src/main/java/it/unisa/diem/AddressBookApplication.java
+++ addressbook/src/main/java/it/unisa/diem/AddressBookApplication.java
@@ -1,43 +1,41 @@
 package it.unisa.diem;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
-import java.io.File;
 import java.io.IOException;
 
-import it.unisa.diem.Utility.FileManager;
-import it.unisa.diem.Utility.SceneManager;
-
 /**
- * The main class of the application, which loads the assets and starts the GUI.
- * @see Application
- */
+* JavaFX App
+*/
 public class AddressBookApplication extends Application {
 
-    @Override
-    public void start(Stage stage) throws IOException {
+   private static Scene scene;
 
-        FileManager.createPaths();
-        if(!new File(FileManager.profileListPath).exists()){
-            try {
-                Files.createFile(Paths.get(FileManager.profileListPath));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+   @Override
+   public void start(Stage stage) throws IOException {
+       scene = new Scene(loadFXML("ProfileSelection"));
+       stage.setScene(scene);
+       stage.initStyle(StageStyle.UNDECORATED);
+       stage.setFullScreen(true);
+       stage.show();
+   }
 
-        try{SceneManager.loadProfileSelection(stage);}catch(IOException e){System.exit(1);}
-    }
+   static void setRoot(String fxml) throws IOException {
+       scene.setRoot(loadFXML(fxml));
+   }
 
-    public static void main(String[] args) {
-        launch();
-    }
+   public static Parent loadFXML(String fxml) throws IOException {
+       FXMLLoader fxmlLoader = new FXMLLoader(AddressBookApplication.class.getResource(fxml + "View.fxml"));
+       return fxmlLoader.load();
+   }
+
+   public static void main(String[] args) {
+       launch();
+   }
 
 }
